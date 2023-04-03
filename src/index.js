51b57069e1422e6d06867c9e61ac2105a111d056
Groune <98833627+gregoirevaillant@users.noreply.github.com>
2023-04-03 22:21:11 +0200

    get updated from most recent news update.

---
+++ src/index.js
@@ -0,0 +1,122 @@
+// SELECT THE INTERACTIVE ELEMENTS
+const addTaskform = document.querySelector("#addTaskForm");
+const addTaskButton = document.querySelector("#addTaskButton");
+
+// SELECT THE MODAL ELEMENTS
+const addTaskModal = document.querySelector("#addTaskModal");
+
+// ADD EVENT LISTENERS
+addTaskButton.addEventListener("click", () => {
+    addTaskModal.style.display = "flex";
+});
+
+addTaskform.addEventListener("submit", addTask);
+
+function addTask(event) {
+    event.preventDefault();
+    const title = document.querySelector("#addTaskTitle").value;
+    const description = document.querySelector("#addTaskDescription").value;
+    const id = Math.floor(Math.random() * 100000000);
+    tasks.push(new Task(title, description, id));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    addTaskModal.style.display = "none";
+    displayTasks();
+}
+
+function displayTasks() {
+    const tasksContainer = document.querySelector("#tasksContainer");
+    const taskCards = document.querySelectorAll(".taskCard");
+    taskCards.forEach((taskCard) => {
+        taskCard.remove();
+    });
+    tasks.forEach((task) => {
+        const taskCard = document.createElement("div");
+        taskCard.classList.add("taskCard");
+        taskCard.innerHTML = `
+            <div class="taskCardTitle">
+                <h3>${task.title}</h3>
+            </div>
+            <div class="taskCardDescription">
+                <p>${task.description}</p>
+            </div>
+            <div class="taskCardActions">
+                <button class="taskCardEditButton" id="${task.id}">Edit</button>
+                <button class="taskCardDeleteButton" id="${task.id}" >Delete</button>
+            </div>  
+        `;
+        if (tasksContainer) {
+            tasksContainer.appendChild(taskCard);
+        }
+        // DELETE THE TASKS
+        let deleteButtons = document.querySelectorAll(".taskCardDeleteButton");
+        deleteButtons.forEach((deleteButton) => {
+            deleteButton.addEventListener("click", (event) => {
+                const id = event.target.id;
+                tasks.forEach((task, index) => {
+                    if (task.id == id) {
+                        tasks.splice(index, 1);
+                        localStorage.setItem("tasks", JSON.stringify(tasks));
+                        displayTasks();
+                    }
+                });
+            });
+        });
+        // EDIT THE TASKS
+        let editButtons = document.querySelectorAll(".taskCardEditButton");
+        editButtons.forEach((editButton) => {
+            editButton.addEventListener("click", (event) => {
+                const id = event.target.id;
+                const taskIndex = tasks.findIndex((task) => task.id == id);
+                const task = tasks[taskIndex];
+                const editTaskForm = document.querySelector("#editTaskForm");
+                const editTaskTitle = document.querySelector("#editTaskTitle");
+                const editTaskDescription = document.querySelector(
+                    "#editTaskDescription"
+                );
+
+                editTaskTitle.value = task.title;
+                editTaskDescription.value = task.description;
+
+                const editTaskModal = document.querySelector("#editTaskModal");
+                editTaskModal.style.display = "flex";
+
+                editTaskForm.addEventListener("submit", (event) => {
+                    event.preventDefault();
+                    const newTitle = editTaskTitle.value;
+                    const newDescription = editTaskDescription.value;
+                    tasks[taskIndex] = new Task(newTitle, newDescription, id);
+                    localStorage.setItem("tasks", JSON.stringify(tasks));
+                    displayTasks();
+                    editTaskModal.style.display = "none";
+                });
+
+                const editTaskModalCloseButton =
+                    document.querySelector(".close");
+                editTaskModalCloseButton.addEventListener("click", () => {
+                    editTaskModal.style.display = "none";
+                });
+            });
+        });
+    });
+}
+
+// RETRIEVE THE STORED TASKS
+let tasks = [];
+
+// LOAD THE TASKS FROM LOCAL STORAGE ON PAGE LOAD
+window.addEventListener("load", () => {
+    const tasksFromLocalStorage = localStorage.getItem("tasks");
+    if (tasksFromLocalStorage) {
+        tasks = JSON.parse(tasksFromLocalStorage);
+        displayTasks();
+    }
+});
+
+// CREATE A TASK CLASS
+class Task {
+    constructor(title, description, id) {
+        this.title = title;
+        this.description = description;
+        this.id = id;
+    }
+}
