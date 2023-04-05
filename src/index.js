38f886926b7ccc67c0ff50eedfdd4e03cb1aa6dd
Groune <98833627+gregoirevaillant@users.noreply.github.com>
2023-04-05 20:37:54 +0200

    fix the problems

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
+// SELECT THE INTERACTIVE ELEMENTS
 const addTaskform = document.querySelector("#addTaskForm");
 const addTaskButton = document.querySelector("#addTaskButton");
-const addTaskModal = document.querySelector("#addTaskModal");
-const editTaskModal = document.querySelector("#editTaskModal");
-const tasksContainer = document.querySelector("#tasksContainer");
-const taskCards = document.querySelectorAll(".taskCard");
-let deleteButtons = document.querySelectorAll(".taskCardDeleteButton");
-let editButtons = document.querySelectorAll(".taskCardEditButton");
-const projectsContainer = document.querySelector("#projectsContainer");
-const projectButtons = document.querySelectorAll(".projectButton");
 
+// SELECT THE MODAL ELEMENTS
+const addTaskModal = document.querySelector("#addTaskModal");
+
+// ADD EVENT LISTENERS
 addTaskButton.addEventListener("click", () => {
     addTaskModal.style.display = "flex";
 });
@@ -16,29 +13,19 @@ addTaskButton.addEventListener("click", () => {
 addTaskform.addEventListener("submit", addTask);
 
 function addTask(event) {
-    const taskTitle = document.querySelector("#taskTitle").value;
-    const taskDescription = document.querySelector("#taskDescription").value;
-    const taskDueDate = document.querySelector("#taskDueDate").value;
-    const taskPriority = document.querySelector("#taskPriority").value;
-    const taskProject = document.querySelector("#taskProject").value;
-    const taskId = Math.floor(Math.random() * 100000000);
     event.preventDefault();
-    tasks.push(
-        new Task(
-            taskTitle,
-            taskDescription,
-            taskDueDate,
-            taskPriority,
-            taskProject,
-            taskId
-        )
-    );
+    const title = document.querySelector("#taskTitle").value;
+    const description = document.querySelector("#taskDescription").value;
+    const id = Math.floor(Math.random() * 100000000);
+    tasks.push(new Task(title, description, id));
     localStorage.setItem("tasks", JSON.stringify(tasks));
     addTaskModal.style.display = "none";
     displayTasks();
 }
 
 function displayTasks() {
+    const tasksContainer = document.querySelector("#tasksContainer");
+    const taskCards = document.querySelectorAll(".taskCard");
     taskCards.forEach((taskCard) => {
         taskCard.remove();
     });
@@ -46,29 +33,31 @@ function displayTasks() {
         const taskCard = document.createElement("div");
         taskCard.classList.add("taskCard");
         taskCard.innerHTML = `
-            <div class="taskCardTitle">
-                <h3>${task.title}</h3>
-            </div>
-            <div class="taskCardDescription">
-                <p>${task.description}</p>
-            </div>
-            <div class="taskCardDueDate">
-                <p><b>Due Date:</b> ${task.date}</p>
-            </div>
-            <div class="taskCardPriority">
-                <p><b>Priority:</b> ${task.priority}</p>
-            </div>
-            <div class="taskCardProject">
-                <p><b>Project:</b> ${task.project}</p>
-            </div>
-            <div class="taskCardActions">
-                <button class="taskCardEditButton" id="${task.id}">Edit</button>
-                    <button class="taskCardDeleteButton" id="${task.id}" >Delete</button>
-            </div>  
+        <div class="taskCardTitle">
+        <h3>${task.title}</h3>
+    </div>
+    <div class="taskCardDescription">
+        <p>${task.description}</p>
+    </div>
+    <div class="taskCardDueDate">
+        <p><b>Due Date:</b> ${task.date}</p>
+    </div>
+    <div class="taskCardPriority">
+        <p><b>Priority:</b> ${task.priority}</p>
+    </div>
+    <div class="taskCardProject">
+        <p><b>Project:</b> ${task.project}</p>
+    </div>
+    <div class="taskCardActions">
+        <button class="taskCardEditButton" id="${task.id}">Edit</button>
+            <button class="taskCardDeleteButton" id="${task.id}" >Delete</button>
+    </div>      
         `;
         if (tasksContainer) {
             tasksContainer.appendChild(taskCard);
         }
+        // DELETE THE TASKS
+        let deleteButtons = document.querySelectorAll(".taskCardDeleteButton");
         deleteButtons.forEach((deleteButton) => {
             deleteButton.addEventListener("click", (event) => {
                 const id = event.target.id;
@@ -81,6 +70,8 @@ function displayTasks() {
                 });
             });
         });
+        // EDIT THE TASKS
+        let editButtons = document.querySelectorAll(".taskCardEditButton");
         editButtons.forEach((editButton) => {
             editButton.addEventListener("click", (event) => {
                 const id = event.target.id;
@@ -91,39 +82,26 @@ function displayTasks() {
                 const editTaskDescription = document.querySelector(
                     "#editTaskDescription"
                 );
-                const editTaskDueDate =
-                    document.querySelector("#editTaskDueDate");
-                const editTaskPriority =
-                    document.querySelector("#editTaskPriority");
-                const editTaskProject =
-                    document.querySelector("#editTaskProject");
 
                 editTaskTitle.value = task.title;
                 editTaskDescription.value = task.description;
-                editTaskDueDate.value = task.date;
-                editTaskPriority.value = task.priority;
-                editTaskProject.value = task.project;
 
+                const editTaskModal = document.querySelector("#editTaskModal");
                 editTaskModal.style.display = "flex";
 
                 editTaskForm.addEventListener("submit", (event) => {
                     event.preventDefault();
                     const newTitle = editTaskTitle.value;
                     const newDescription = editTaskDescription.value;
-                    const newDueDate = editTaskDueDate.value;
-                    const newPriority = editTaskPriority.value;
-                    const newProject = editTaskProject.value;
-
-                    tasks[taskIndex] = new Task(
-                        newTitle,
-                        newDescription,
-                        newDueDate,
-                        newPriority,
-                        newProject,
-                        id
-                    );
+                    tasks[taskIndex] = new Task(newTitle, newDescription, id);
                     localStorage.setItem("tasks", JSON.stringify(tasks));
                     displayTasks();
+                    editTaskModal.style.display = "none";
+                });
+
+                const editTaskModalCloseButton =
+                    document.querySelector(".close");
+                editTaskModalCloseButton.addEventListener("click", () => {
                     editTaskModal.style.display = "none";
                 });
             });
@@ -131,76 +109,23 @@ function displayTasks() {
     });
 }
 
-function addProject(event) {
-    event.preventDefault();
-    const project = document.querySelector("#addProjectTitle").value;
-    projects.push(new Project(project));
-    localStorage.setItem("projects", JSON.stringify(projects));
-    displayProjects();
-    console.log(projects);
-}
+// RETRIEVE THE STORED TASKS
+let tasks = [];
 
-function displayProjects() {
-    projectButtons.forEach((projectButton) => {
-        projectButton.remove();
-    });
-    projects.forEach((project) => {
-        const projectButton = document.createElement("button");
-        projectButton.classList.add("projectButton");
-        projectButton.innerHTML = `
-            <span>${project.project}</span>
-
-        `;
-        if (projectsContainer) {
-            projectsContainer.appendChild(projectButton);
-        }
-    });
-}
-
-const addProjectForm = document.querySelector("#addProjectForm");
-addProjectForm.addEventListener("submit", addProject);
-
+// LOAD THE TASKS FROM LOCAL STORAGE ON PAGE LOAD
 window.addEventListener("load", () => {
     const tasksFromLocalStorage = localStorage.getItem("tasks");
-    const projectsFromLocalStorage = localStorage.getItem("projects");
     if (tasksFromLocalStorage) {
         tasks = JSON.parse(tasksFromLocalStorage);
         displayTasks();
-    } else if (projectsFromLocalStorage) {
-        projects = JSON.parse(projectsFromLocalStorage);
-        displayProjects();
     }
 });
 
-window.addEventListener("click", (event) => {
-    if (event.target == addTaskModal) {
-        addTaskModal.style.display = "none";
-    } else if (event.target == editTaskModal) {
-        editTaskModal.style.display = "none";
-    }
-});
-
-// STORE THE TASKS
-let tasks = [];
-
-// STORE THE PROJECTS
-let projects = [];
-
-// TASK CLASS
+// CREATE A TASK CLASS
 class Task {
-    constructor(title, description, date, priority, project, id) {
+    constructor(title, description, id) {
         this.title = title;
         this.description = description;
-        this.date = date;
-        this.priority = priority;
-        this.project = project;
         this.id = id;
-    }
-}
-
-// PROJECT CLASS
-class Project {
-    constructor(project) {
-        this.project = project;
     }
 }
